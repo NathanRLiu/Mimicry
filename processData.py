a043db6140285dac44d705d491996bdd23229cf6
NathanRLiu <nathanliu17@gmail.com>
2021-06-17 13:00:11 -0700

    Input/output sorting

--- processData.py
+++ processData.py
@@ -1,16 +1,45 @@
 import json
 import config
+import random
 
 channelID,serverID,targetID = config.getScope()
 daTestDoc = open("TestDoc","r",encoding="utf-8")
-inputList = open("inputList","w",encoding="utf-8")
+inputListDoc = open("inputList","w",encoding="utf-8")
+outputListDoc = open("outputList","w",encoding="utf-8")
+inputList = []
+outputList = []
 messageDict = {}
 daMessageList = eval(daTestDoc.read())
-for message in daMessageList:
-	if message["author"]["id"] == targetID:
-		if message.get("message_reference") != None:
-			print(message["referenced_message"])
-			message["referenced_message"]["content"]
+print(len(daMessageList))
+for i in range(len(daMessageList)):
+	message = daMessageList[i]
+	if message["author"]["id"] == str(targetID):
+		if type(message.get("referenced_message"))!=dict:
+			print("not a reply")
+			print(i)
+			print(len(daMessageList))
+			if len(daMessageList) != i:
+				outputList.append(message["content"])
+				message = daMessageList[i+1]
+				inputList.append(message["content"])
+			else:
+				pass
 		else:
-			print("is not reply")
+			print(type(message.get("referenced_message")))
+			print(message.get("referenced_message"))
+			inputList.append(message["referenced_message"]["content"])
+			outputList.append(message["content"])
+
+print(len(outputList))
+print(len(inputList))
+myIndice = random.randint(0,len(inputList))
+print(myIndice)
+print("input: "+ inputList[myIndice])
+print("output: "+ outputList[myIndice])
+
+inputListDoc.write(str(inputList),"utf-8")
+outputListDoc.write(str(inputList),"utf-8")
+
 daTestDoc.close()
+inputListDoc.close()
+outputListDoc.close()

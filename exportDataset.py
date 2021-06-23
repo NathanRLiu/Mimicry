c07b396c01aa9919253736a9953fe1995d8f920b
NathanRLiu <nathanliu17@gmail.com>
2021-06-22 18:24:25 -0700

    Data preprocessing
    *Tokenization
    *merge consecutive messages from same targetID
    *sequence padding

--- exportDataset.py
+++ exportDataset.py
@@ -1,13 +1,10 @@
 import json
 import discAutoMsg.discord_automessage as discAutoMsg
 import config
+import pickle
 channelID, serverID, targetID = config.getScope()
-# trainingData = open("Training_Data","w")
-daChannel = discAutoMsg.searchMessages(serverID,False,channel_id=channelID)
-#daChannel = discAutoMsg.searchMessages(channelID,True)#this is for dm
-totalResults = json.loads(daChannel)["total_results"]
-print(totalResults)
 daTestDoc = discAutoMsg.exportMessages(channelID)
-daTestDoc = open(daTestDoc.name,"r",encoding="utf-8")
+
+daTestDoc = pickle.load(daTestDoc.name,"r",encoding="utf-8")
 print(len(eval(daTestDoc.read())))
 daTestDoc.close()
